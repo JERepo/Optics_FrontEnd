104d0d95bea753a32d1e588996f7b0ea8c4aba24
Srinivas-dev995 <elakotisrinu48@gmail.com>
2025-10-17 11:11:20 +0530

    updated code

--- src/api/customBaseQuery.js
+++ src/api/customBaseQuery.js
@@ -6,8 +6,9 @@ const baseQuery = fetchBaseQuery({
   baseUrl:
     import.meta.env.MODE === "development"
       ? import.meta.env.VITE_LOCAL
-      : "http://uat.opticstest.in",
+      // : "http://uat.opticstest.in",
       // : "https://site62300-r9514o.scloudsite101.com",
+      :"https://yourposbuddy.in",
   credentials: "include",
   prepareHeaders: (headers, { getState, endpoint }) => {
     const token = getState().auth.token;
