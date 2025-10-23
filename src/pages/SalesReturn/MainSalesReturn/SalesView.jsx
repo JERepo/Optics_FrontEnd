d3f22a6402b67672b5bcfc1d5348b7ca1ee9ccec
Srinivas-dev995 <elakotisrinu48@gmail.com>
2025-10-23 15:55:29 +0530

    updated sales view

--- src/pages/SalesReturn/MainSalesReturn/SalesView.jsx
+++ src/pages/SalesReturn/MainSalesReturn/SalesView.jsx
@@ -436,12 +436,13 @@ const SalesView = () => {
 
   const totalGST = salesDetails?.data.reduce((sum, item) => {
     const price = parseFloat(item.ReturnPricePerUnit);
+    const qty = parseInt(item.ReturnQty)
     const totalPriceGst = parseFloat(
       calculateGST(price, parseFloat(item.GSTPercentage)).gstAmount
     );
     const fittingPrice = parseFloat(item.FittingCharges || 0);
     const gst = parseFloat(item.FittingGSTPercentage || 0);
-    const fittingGst = fittingPrice * (gst / 100);
+    const fittingGst = (fittingPrice * qty) * (gst / 100);
     return sum + totalPriceGst + fittingGst;
   }, 0);
 
